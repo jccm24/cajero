c99e99a14dd6a2f87632b751eef70b01f9c2a5a9
jccm24 <118667729+jccm24@users.noreply.github.com>
2023-07-04 15:49:20 -0500

    Tercer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var cuentas = [
     { nombre: "Mali", saldo: 200 },
-    { nombre: "Gera", saldo: 290 },
+    { nombre: "Gera", saldo: 290 }, 
     { nombre: "Maui", saldo: 67 }
 ];
 var cuentaSeleccionada = null;
